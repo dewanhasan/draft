5d367fcfd9c5cf22228c4cccbd3fa0251baa3c7d
dewanhasan99 <g00410156@atu.ie>
2023-11-07 09:28:17 +0000

    dsd

---
+++ src/test/java/ie/atu/draftwork/DraftworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.draftwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraftworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
